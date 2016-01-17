bbed0fcfe3dfd7261ce3a8f1ffc8940c5cb032aa
Uğur Yörük <uguryoruk26@gmail.com>
2016-01-17 04:34:26 -0800

    updates

--- apps/stories/models.py
+++ apps/stories/models.py
@@ -10,32 +10,32 @@ class Displayable(models.Model):
     sort_order = models.IntegerField(default=1)
 
     class Meta:
-    	abstract = True
-    	ordering = ("sort_order",)
+        abstract = True
+        ordering = ("sort_order",)
 
     def __unicode__(self):
-    	return u"%s" % self.name
+        return u"%s" % self.name
 
 
 class Story(Displayable):
-	pass
+    pass
 
 
 class Chapter(Displayable):
-	story = models.ForeignKey(Story, related_name="related_chapters")
+    story = models.ForeignKey(Story, related_name="related_chapters")
 
 
 class Snippet(Displayable):
-	chapter = models.ForeignKey(Chapter, related_name="related_snippets")
+    chapter = models.ForeignKey(Chapter, related_name="related_snippets")
 
-	is_completed = models.BooleanField(default=False)
-	is_failed = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
+    is_failed = models.BooleanField(default=False)
 
-	starting_point = models.BooleanField(default=False)
-	checkpoint = models.BooleanField(default=False)
+    starting_point = models.BooleanField(default=False)
+    checkpoint = models.BooleanField(default=False)
 
 
 class Choice(Displayable):
-	source = models.ForeignKey(Snippet, related_name="sources", null=True, blank=True)
-	target = models.ForeignKey(Snippet, related_name="targets", null=True, blank=True)
+    source = models.ForeignKey(Snippet, related_name="targets", null=True, blank=True)
+    target = models.ForeignKey(Snippet, related_name="sources", null=True, blank=True)
 
--- madcyoa/urls.py
+++ madcyoa/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 
 from apps.stories.views import ChapterDetail
 
+
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
@@ -26,9 +27,13 @@ urlpatterns = [
     url(r'^test/$', 'apps.main.views.test'),
 
     url(r'^chapter/(?P<slug>[\w-]+)/$', ChapterDetail.as_view(), name="chapter-detail"),
+    url(r'^snippet/(?P<slug>[\w-]+)/edit/$', 'apps.stories.views.edit_snippet', name="edit-snippet"),
     
     url(r'^snippet/update/$', 'apps.stories.views.update_snippet', name="update-snippet"),
     url(r'^snippet/add_target_choice/$', 'apps.stories.views.add_target_choice', name="add-target-choice"),
-    url(r'^snippet/(?P<slug>[\w-]+)/edit/$', 'apps.stories.views.edit_snippet', name="edit-snippet"),
-
+    url(r'^snippet/add_source_choice/$', 'apps.stories.views.add_source_choice', name="add-source-choice"),
+    url(r'^snippet/delete_choice/$', 'apps.stories.views.delete_choice', name="delete-choice"),
+    url(r'^snippet/update_source_choice/$', 'apps.stories.views.update_source_choice', name="update-source-choice"),
+    url(r'^snippet/update_target_choice/$', 'apps.stories.views.update_target_choice', name="update-target-choice"),
+    
 ]
